3c0e2f9b5e23ae7c4ab18ce8ec93fe2a81466b22
rssantos013 <santos.raphael08@icloud.com>
2024-10-29 12:20:38 -0700

    Add files via upload

---
+++ alura.js
@@ -0,0 +1,24 @@
+(function() {
+    'use strict';
+
+    const actualUrl = window.location.href;
+    const nextLessonButton = document.getElementsByClassName("bootcamp-next-button")[0];
+
+    if (nextLessonButton) {
+        const parts = actualUrl.split('/');
+        const lessonName = parts[4];
+        const lessonId = parts[6];
+
+        fetch(`https://cursos.alura.com.br/course/${lessonName}/task/${lessonId}/mark-video`, {
+            method: 'POST',
+            credentials: 'include',
+            headers: {
+                'Content-Type': 'application/json',
+                'Cookie': document.cookie
+            }
+        });
+
+        setTimeout(() => nextLessonButton.click(), 4000);
+    } else {
+    }
+})();
